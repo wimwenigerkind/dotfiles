577b7ffb349b74d9d10df6cb8d8d99c9354f361d
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-04-11 14:12:08 +0200

    Add custom Zsh scripts and aliases for improved workflow

---
+++ .zshrc-scripts/aliases.zsh
@@ -0,0 +1,19 @@
+# Alias Section
+alias wim='vim'
+alias d='docker'
+alias bc='bin/console'
+alias bcdmm='bin/console doctrine:migrations:migrate'
+alias goDev='cd ~/Development/'
+alias goOneDev='cd ~/OneDrive/development/'
+alias goSym='cd ~/SymfonyProjects/'
+alias ll='ls -la'
+alias up='cd ..'
+alias ww='vim'
+
+# Configs
+alias sshconfig='vim ~/.ssh/config'
+alias zshrcconfig='vim ~/.zshrc'
+
+# Reload Configs
+alias zshrcreload='source ~/.zshrc'
+alias sc='zshrcreload'
