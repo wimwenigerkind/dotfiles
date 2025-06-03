e32fd833f05ead22d171d4f064bf6e34aca46d1f
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-06-03 23:14:07 +0200

    Update aliases.zsh with new Composer and Symfony aliases

--- .zshrc-scripts/aliases.zsh
+++ .zshrc-scripts/aliases.zsh
@@ -1,8 +1,6 @@
 # Alias Section
 alias wim='vim'
 alias d='docker'
-alias bc='bin/console'
-alias bcdmm='bin/console doctrine:migrations:migrate'
 alias goDev='cd ~/Development/'
 alias goOneDev='cd ~/OneDrive/development/'
 alias goSym='cd ~/SymfonyProjects/'
@@ -10,10 +8,20 @@ alias ll='ls -la'
 alias up='cd ..'
 alias ww='vim'
 
+## Composer
+alias c='composer'
+alias cfix='composer update --lock --no-install --no-scripts'
+
+## Symfony / Shopware
+alias bbs='bin/build-storefront.sh'
+alias bba='bin/build-administration.sh'
+alias bc='bin/console'
+alias bcdmm='bin/console doctrine:migrations:migrate'
+
 # Configs
 alias sshconfig='vim ~/.ssh/config'
 alias zshrcconfig='vim ~/.zshrc'
 
 # Reload Configs
-alias zshrcreload='source ~/.zshrc'
-alias sc='zshrcreload'
+alias zshrcreload='zsh'
+alias sc='zsh'
