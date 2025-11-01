c21d73072c9b67a83eed3a1d08f202707090bb2b
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-11-01 19:40:40 +0100

    refactor exports

---
+++ .zsh/export.zsh
@@ -0,0 +1,9 @@
+export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
+export OPENSSL_CONF="/Users/wim.wenigerkind/Library/Application Support/Herd/config/php/openssl.cnf"
+export PATH="/opt/homebrew/bin:$PATH"
+
+# bun
+[ -s "/Users/wim.wenigerkind/.bun/_bun" ] && source "/Users/wim.wenigerkind/.bun/_bun"
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+export PATH="/Users/wim.wenigerkind/.bin:$PATH"
