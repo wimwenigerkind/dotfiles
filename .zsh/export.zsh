8888ffdce3e69b6685dd8a52dfd25fdbe0f26d8d
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-02-12 23:00:39 +0100

    Update export.zsh to include local bin directory in PATH

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -7,3 +7,5 @@ export PATH="/opt/homebrew/bin:$PATH"
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 export PATH="/Users/wim.wenigerkind/.bin:$PATH"
+
+export PATH="$HOME/.local/bin:$PATH"
