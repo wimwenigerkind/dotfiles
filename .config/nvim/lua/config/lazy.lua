c216cec226639be2feeb2732033d27814904d167
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-03-05 20:44:25 +0100

    Add Copilot plugin to LazyVim configuration

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- extras
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
